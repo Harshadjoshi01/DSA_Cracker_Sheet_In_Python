c0381a2e419fa9318f392fb450b889677e2d132f
Harshad Joshi <joshiharshad41@gmail.com>
2022-05-03 19:31:20 +0530

    Reverse the Array Done

--- Arrays/ReverseTheArray.py
+++ Arrays/ReverseTheArray.py
@@ -1,3 +1,16 @@
+'''
+Given an array (or string), the task is to reverse the array/string.
+Examples : 
+ 
+Input  : arr[] = {1, 2, 3}
+Output : arr[] = {3, 2, 1}
+
+Input :  arr[] = {4, 5, 1, 2}
+Output : arr[] = {2, 1, 5, 4}
+
+'''
+
+
 def reverseTheArray(arr):
     for i in range(len(arr)//2):
         arr[i], arr[len(arr)-i-1] = arr[len(arr)-i-1], arr[i]
