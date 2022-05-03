20a139d6533ded6f18fad4cc348429f5f146f7d3
Harshad Joshi <joshiharshad41@gmail.com>
2022-05-03 19:52:14 +0530

    MovenegativeToOneSide Done

--- Arrays/MovenegativeToOneSideArray.py
+++ Arrays/MovenegativeToOneSideArray.py
@@ -1,3 +1,14 @@
+'''
+An array contains both positive and negative numbers in random order. Rearrange the array elements so that all negative numbers appear before all positive numbers.
+
+Examples : 
+
+Input: -12, 11, -13, -5, 6, -7, 5, -3, -6
+Output: -12 -13 -5 -7 -3 -6 11 6 5
+Note: Order of elements is not important here.
+
+'''
+
 def movenegativeToOneSideArray(arr):
     left = 0
     right = len(arr) - 1
